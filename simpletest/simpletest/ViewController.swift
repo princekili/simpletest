c26f8147e3d1b25377e4097784ac7fbad823360f
michael <michaelchang@nine-yi.com>
2021-01-15 22:16:10 +0800

    [feat] Create project.

---
+++ simpletest/simpletest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpletest
+//
+//  Created by Michael Chang on 2021/1/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
