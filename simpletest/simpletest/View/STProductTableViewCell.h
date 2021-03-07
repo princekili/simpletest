8569d378f8e9bcaced0ca3b336ad3dd93c048964
Prince Wang <princekili0422@gmail.com>
2021-03-08 02:07:30 +0800

    Create STProductTableViewCell files (.h & .m)

---
+++ simpletest/simpletest/View/STProductTableViewCell.h
@@ -0,0 +1,12 @@
+//
+//  STProductTableViewCell.h
+//  simpletest
+//
+//  Created by Prince Wang on 2021/3/8.
+//
+
+#ifndef STProductTableViewCell_h
+#define STProductTableViewCell_h
+
+
+#endif /* STProductTableViewCell_h */
---
+++ simpletest/simpletest/View/STProductTableViewCell.m
@@ -0,0 +1,8 @@
+//
+//  STProductTableViewCell.m
+//  simpletest
+//
+//  Created by Prince Wang on 2021/3/8.
+//
+
+#import <Foundation/Foundation.h>
